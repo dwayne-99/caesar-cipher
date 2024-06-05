497795769ffce0e81b9dae9c52ea48d70c78ba65
dwayne-99 <121464672+dwayne-99@users.noreply.github.com>
2024-06-05 16:22:38 -0400

    Update Caesar Cipher.py

--- Caesar Cipher.py
+++ Caesar Cipher.py
@@ -17,16 +17,23 @@ def caesar(start_text, shift_amount, cipher_direction):
     
 # If direction is decode multiply the shift amount by -1 to shift in the oppsoite direction
   
-  for letter in start_text:
-    position = alphabet.index(letter)
+  for char in start_text:
+    if char in alphabet:
+      position = alphabet.index(letter)
 
-# Find the position of the current letter in the alphabet list
+# Find the position of the current character in the alphabet list if the user input is a letter of the alphabet
     
-    new_position = position + shift_amount
+      new_position = position + shift_amount
 # Calculate the new position by adding the shift amount to the current position
     
-    end_text += alphabet[new_position]
+      end_text += alphabet[new_position]
 # Append the letter at the new position to the end_text string
+
+    else:
+      end_text += char
+      
+# Appends the actual character the user inputs to the end_text string
+# if the user input is not a letter (blank spaces, non alphabetical symbols, etc.)
   
   print(f"Here's the {direction}d result: {end_text}")
 
